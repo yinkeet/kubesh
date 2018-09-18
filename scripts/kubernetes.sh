67de4cae20a5714e2bcd2ecbc4e8269434527275
Yin Keet <yinkeet.wong@imoney-group.com>
2018-09-18 21:28:45 +0800

    Using old code, falling back

--- scripts/kubernetes.sh
+++ scripts/kubernetes.sh
@@ -68,29 +68,40 @@ image_pull_secret() {
 	 	kubernetes_image_pull_secret_help
     fi
     
-    # local filename=$1
-    # image_pull_secret_key="$PWD/$filename"
-    # if [ ! -f $image_pull_secret_key ]; then
-    #     echo "'$image_pull_secret_key' not found!"
-    #     exit
-    # fi
+    local filename=$1
+    local image_pull_secret_key="$PWD/$filename"
+    if [ ! -f $image_pull_secret_key ]; then
+        echo "'$image_pull_secret_key' not found!"
+        exit
+    fi
     
-    # Load image_pull_secret.yaml
-    local image_pull_secret_yaml="$PWD/image_pull_secret.yaml"
-    if [ ! -f $image_pull_secret_yaml ]; then
-        image_pull_secret_yaml="$HOME_DIR/image_pull_secret.yaml"
-    fi
+    # # Load image_pull_secret.yaml
+    # local image_pull_secret_yaml="$PWD/image_pull_secret.yaml"
+    # if [ ! -f $image_pull_secret_yaml ]; then
+    #     image_pull_secret_yaml="$HOME_DIR/image_pull_secret.yaml"
+    # fi
 
-    # Load service_account.yaml
-    local service_account_yaml="$PWD/service_account.yaml"
-    if [ ! -f $service_account_yaml ]; then
-        service_account_yaml="$HOME_DIR/service_account.yaml"
-    fi
+    # # Load service_account.yaml
+    # local service_account_yaml="$PWD/service_account.yaml"
+    # if [ ! -f $service_account_yaml ]; then
+    #     service_account_yaml="$HOME_DIR/service_account.yaml"
+    # fi
 
-    # local BASE64_ENCODED_IMAGE_PULL_SECRET_JSON=$(base64 $image_pull_secret_key)
-	local BASE64_ENCODED_IMAGE_PULL_SECRET_JSON=$1
-	envsubst < $image_pull_secret_yaml | kubectl replace -f -
-	cat $service_account_yaml | kubectl replace -f -
+    # # local BASE64_ENCODED_IMAGE_PULL_SECRET_JSON=$(base64 $image_pull_secret_key)
+	# local BASE64_ENCODED_IMAGE_PULL_SECRET_JSON=$1
+	# envsubst < $image_pull_secret_yaml | kubectl replace -f -
+	# cat $service_account_yaml | kubectl replace -f -
+	secretName="gcr-json-key"
+    metadataName=$(kubectl get secret $secretName -o json | jq '.metadata.name')
+    metadataName="${metadataName%\"}"
+	metadataName="${metadataName#\"}"
+    if [ "$metadataName" == "$secretName" ]; then
+        kubectl delete secret $secretName
+    fi
+    password="$(cat $image_pull_secret_key)"
+    kubectl create secret docker-registry gcr-json-key --docker-server=https://gcr.io --docker-username=_json_key --docker-password="$password" --docker-email=tech@imoney-group.com
+    kubectl delete serviceaccount default 
+    kubectl patch serviceaccount default -p "{\"imagePullSecrets\":[{\"name\":\"$secretName\"}]}"
 }
 
 if [[ $1 = $KUBERNETES ]]; then
