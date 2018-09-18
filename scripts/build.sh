650ab4b8c7bc71e163d2ddcbc1af852633de6172
Yin Keet <yinkeet.wong@imoney-group.com>
2018-09-18 09:56:57 +0800

    Refactored codes to be much cleaner

--- scripts/build.sh
+++ scripts/build.sh
@@ -34,9 +34,10 @@ build_kube_arguments_parser() {
 		return 1
 	fi
 
-	for i in "$@"
+	local argument
+	for argument in "$@"
 	do
-	case $i in
+	case $argument in
 		-h|--help)
 		return 0
 		;;
@@ -66,50 +67,32 @@ build_dev() {
 }
 
 build_minikube_image() {
-    environment=$1
-    container=$2
+    local environment=$1
+    local container=$2
 
+	local dockerfile
     dockerfile=$(get_config $environment "_DOCKERFILE" ${MINIKUBE_ENVIRONMENTS[@]})
     if [ $? -eq 0 ]; then
         echo "Dockerfile filename for $environment not specified!"
         exit
     fi
+
     dockerfile="$PWD/$container/$dockerfile"
     if [ ! -f $dockerfile ]; then
         echo "'$dockerfile' not found!"
         exit
     fi
-	image_name=$(generate_image_name $environment $container)
-    # tag="${container_key}_TAG"
-    # tag=${!tag}
-    # image_name="$APP/$container_name:$tag"
-    docker build --force-rm --no-cache --rm --file $dockerfile -t $image_name $PWD
-}
 
-build_minikube() {
-    local environment=$1
-	local container
-    shift
-    build_kube_arguments_parser $@
-	local result=$?
-	if [ $result -eq 0 ]; then
-	 	build_kube_help $MINIKUBE
-	elif [ $result -eq 1 ]; then
-		echo "Building all images for $APP..."
-		for container in "${CONTAINERS[@]}"
-		do
-            build_minikube_image $environment $container
-		done
-	else
-		echo "Building $1 image for $APP..."
-		build_minikube_image $environment $1
-	fi
+	local image_name=$(generate_image_name $environment $container)
+	local tag=$(cat $PWD/$container/tag)
+	local image_name_tag="$image_name:$tag"
+	docker build --force-rm --no-cache --rm --file $dockerfile -t $image_name_tag $PWD
 }
 
 build_image_checker() {
-	image_name=$1
-	tag=$2
-	image_details=$(gcloud container images list-tags $image_name --format=json --filter="tags=$tag" | jq '.[0].tags[0]')
+	local image_name=$1
+	local tag=$2
+	local image_details=$(gcloud container images list-tags $image_name --format=json --filter="tags=$tag" | jq '.[0].tags[0]')
 	image_details="${image_details%\"}"
 	image_details="${image_details#\"}"
 	if [ "$image_details" == "$tag" ]; then
@@ -120,14 +103,14 @@ build_image_checker() {
 }
 
 build_kube_image() {
-    environment=$1
-    container_key=$2
-    container_name=$3
-    tag="${container_key}_TAG"
-    tag=${!tag}
-    image_name="$GCR_IO/$PROJECT_NAME/$APP/$CLUSTER/$container_name"
-    image_name_tag="$image_name:$tag"
-    build_image_checker $image_name $tag
+    local environment=$1
+    local container=$2
+    
+	local image_name=$(generate_image_name $environment $container)
+	local tag=$(cat $PWD/$container/tag)
+	local image_name_tag="$image_name:$tag"
+    
+	build_image_checker $image_name $tag
     if [ $? -eq 1 ]; then
         echo "Image $image_name_tag is already built and pushed to the cloud."
     else
@@ -136,39 +119,57 @@ build_kube_image() {
             echo "Dockerfile filename for $environment not specified!"
             exit
         fi
-        dockerfile="$PWD/$container_name/$dockerfile"
+
+        dockerfile="$PWD/$container/$dockerfile"
         if [ ! -f $dockerfile ]; then
             echo "'$dockerfile' not found!"
             exit
         fi
+
         docker build --force-rm --no-cache --rm -f $dockerfile -t $image_name_tag $PWD
 	    docker push $image_name_tag
     fi
 }
 
 build_kube() {
-    environment=$1
+    local environment=$1
     shift
-	args=$(build_kube_arguments_parser $@)
-	result=$?
-	if [ $result -eq 0 ]; then
+	build_kube_arguments_parser $@
+	if [ $? -eq 0 ]; then
 	 	build_kube_help $environment
-	elif [ $result -eq 1 ]; then
+	elif [ $? -eq 1 ]; then
 		echo "Building all images for $APP..."
-        for i in "${CONTAINERS[@]}"
-		do
-			IFS='=' read -ra container <<< "$i"
-            build_kube_image $environment ${container[0]} ${container[1]}
-		done
+		local container
+
+		contains $environment ${MINIKUBE_ENVIRONMENTS[@]}
+		if [ $? -eq 1 ]; then
+			for container in "${CONTAINERS[@]}"
+			do
+				build_minikube_image $environment $container
+			done
+			exit
+		fi
+
+		contains $environment ${KUBE_ENVIRONMENTS[@]}
+		if [ $? -eq 1 ]; then
+			for container in "${CONTAINERS[@]}"
+			do
+				build_kube_image $environment $container
+			done
+			exit
+		fi
 	else
 		echo "Build and push $1 image for $APP..."
-        dockerfile=$(kube_dockerfile $environment)
-        for i in "${CONTAINERS[@]}"
-		do
-			IFS='=' read -ra container <<< "$i"
-			if [[ $1 = "${container[1]}" ]]; then
-                build_kube_image $environment ${container[0]} ${container[1]}
-			fi
-		done
+		contains $environment ${MINIKUBE_ENVIRONMENTS[@]}
+		if [ $? -eq 1 ]; then
+			build_minikube_image $environment $1
+			exit
+		fi
+
+		contains $environment ${KUBE_ENVIRONMENTS[@]}
+		if [ $? -eq 1 ]; then
+			build_kube_image $environment $1
+			exit
+		fi
 	fi
 }
