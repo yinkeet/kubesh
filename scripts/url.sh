ab54a9c8b7b79ddd52e3610780b4f14b67fa8a7c
Yin Keet <yinkeet.wong@imoney-group.com>
2018-09-18 16:04:18 +0800

    Minikube and kube returns its url respectively

--- scripts/url.sh
+++ scripts/url.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-url_minikube() {
-	minikube service $APP --url
-}
-
 url_kube() {
-	local array=($(kubectl get services --field-selector=metadata.name=$APP -o json | jq '.items[0].status.loadBalancer.ingress[0].ip , .items[0].spec.ports[0].targetPort'))
-	local ip=${array[0]}
-	local port=${array[1]}
-	ip="${ip%\"}"
-	ip="${ip#\"}"
-    echo "http://$ip:$port/"
+	contains $environment ${MINIKUBE_ENVIRONMENTS[@]}
+	if [ $? -eq 1 ]; then
+		minikube service $APP --url
+		exit
+	fi
+
+	contains $environment ${KUBE_ENVIRONMENTS[@]}
+	if [ $? -eq 1 ]; then
+		local array=($(kubectl get services --field-selector=metadata.name=$APP -o json | jq '.items[0].status.loadBalancer.ingress[0].ip , .items[0].spec.ports[0].targetPort'))
+		local ip=${array[0]}
+		local port=${array[1]}
+		ip="${ip%\"}"
+		ip="${ip#\"}"
+		echo "http://$ip:$port/"
+		exit
+	fi
 }
