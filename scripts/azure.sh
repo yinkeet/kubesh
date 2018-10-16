83520c841363e0549ae055a6ae8524ea194c1db3
keet <keet@bambu.life>
2018-10-16 21:12:32 +0800

    Added azure cluster helper

--- scripts/azure.sh
+++ scripts/azure.sh
@@ -14,14 +14,33 @@ azure_help() {
 	exit
 }
 
-azure_auth_help() {
+azure_cluster_help() {
+	local string
+	local array
+	local environments
+	for string in "${KUBE_ENVIRONMENTS[@]}"
+	do
+		IFS='=' read -ra array <<< "$string"
+		local description="${array[0]}_DESCRIPTION"
+		description=${!description}
+		string=$(printf '%-13s%s' "${array[1]}" "$description")
+		if [[ ! $environments ]]; then
+			environments=$string
+		else
+			environments="$environments\n  $string"
+		fi
+	done
+
 	echo ""
-	echo "Usage: ./kubesh $AZURE $AUTH APP_ID PASSWORD TENANT"
+	echo "Usage: ./kubesh $AZURE $CLUSTER_COMMAND ENVIRONMENT"
     echo "       ./kubesh [ -h | --help ]"
 	echo ""
 	echo "Options:"
 	echo "  -h, --help    Prints usage."
 	echo ""
+	echo "Environments:"
+	echo -e "  $environments"
+
 	exit
 }
 
@@ -66,6 +85,28 @@ azure_auth_arguments_parser() {
 	return 1
 }
 
+azure_cluster_arguments_parser() {
+	# No arguments, show help
+	if [ "$#" -eq 0 ]; then
+		return 0
+	fi
+
+	# Find options
+    for i in "$@"
+	do
+	case $i in
+		-h|--help)
+		return 0
+		;;
+		*)
+		;;
+	esac
+	done
+
+	contains $1 ${KUBE_ENVIRONMENTS[@]}
+	return $?
+}
+
 azure_auth() {
     local found_error=false
     if [[ ! $AZURE_APP_ID ]]; then
@@ -87,8 +128,19 @@ azure_auth() {
     if [ "$found_error" = true ] ; then
         exit 1
     fi
-    az login --service-principal -u $appId -p $password --tenant $tenant
+    az login --service-principal -u $AZURE_APP_ID -p $AZURE_PASSWORD --tenant $AZURE_TENANT
     az acr login --name $ACR_NAME
+}
+
+azure_cluster() {
+	azure_cluster_arguments_parser $@
+    if [ $? -eq 0 ]; then
+	 	azure_cluster_help
+    fi
+
+	local environment=$1
+	load_environment_file $environment
+    az aks get-credentials --resource-group $AZURE_RESOURCE_GROUP --name $CLUSTER --overwrite-existing
 }
 
 azure_container_registry_url() {
@@ -104,6 +156,10 @@ if [[ $1 = $AZURE ]]; then
 		if [[ $1 = $AUTH ]]; then
             shift
 			azure_auth
+			exit
+		elif [[ $1 = $CLUSTER_COMMAND ]]; then
+			shift
+			azure_cluster $@
 			exit
 		fi
 	fi
