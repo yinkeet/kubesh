56bcac6481b4c7a505dd1c92e05c5ef8c5a5c3f2
Yin Keet <yinkeet.wong@imoney-group.com>
2018-10-29 02:37:01 +0800

    Added image name template Remove static cleanup and kill

--- library/docker.py
+++ library/docker.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from subprocess import PIPE, call, check_output
 from typing import List
 
@@ -7,6 +8,10 @@ from environment import Environment
 
 
 class Docker(Environment):
+    @property
+    def image_name_template(self):
+        return self.templates.get('docker_image_name', '__DIRECTORY_NAME_____CONTAINER__')
+
     def build(self, containers: List[str]=[]):
         command = ['docker-compose', '-f', self.deployment_filename, 'build']
         if containers:
@@ -28,7 +33,7 @@ class Docker(Environment):
     def clean_up(self, containers: List[str]=[]):
         untagged_images = []
         for container in containers:
-            image_name = os.getcwd().split(os.sep)[-1] + '_' + container
+            image_name = self.image_name_template.replace('__DIRECTORY_NAME__', os.getcwd().split(os.sep)[-1]).replace('__CONTAINER__', container)
             untagged_images.extend (
                 check_output(
                     ['docker', 'images', image_name, '-f', 'dangling=true', '-q']
@@ -54,47 +59,13 @@ class Docker(Environment):
             command.append('-f')
         if containers:
             command.extend(containers)
-        call(command)
+        try:
+            call(command)
+        except KeyboardInterrupt:
+            sys.exit(0)
 
     def ssh(self, pod: str=None, container: str=None, command: str=None, args: List[str]=[]):
         command = ['docker-compose', '-f', self.deployment_filename, 'exec', container, command]
         if args:
             command.extend(args)
         call(command)
-
-    @staticmethod
-    def cleanup():
-        Docker.remove_stopped_containers()
-        Docker.remove_untagged_images()
-        Docker.remove_unused_volumes()
-
-    @staticmethod
-    def remove_stopped_containers():
-        print('Removing stopped containers... ', end='', flush=True)
-        stopped_containers = check_output(
-            ['docker', 'ps', '-a', '-f', 'status=exited', '-q']
-        ).decode('UTF-8').splitlines()
-        
-        if stopped_containers:
-            command = ['docker', 'rm']
-            command.extend(stopped_containers)
-            call(command, stdout=PIPE)
-        print('done')
-
-    @staticmethod
-    def remove_untagged_images():
-        print('Removing untagged images... ', end='', flush=True)
-        call(['docker', 'image', 'prune', '-f'], stdout=PIPE)
-        print('done')
-
-    @staticmethod
-    def remove_unused_volumes():
-        print('Removing unused volumes... ', end='', flush=True)
-        call(['docker', 'volume', 'prune', '-f'], stdout=PIPE)
-        print('done')
-
-    @staticmethod
-    def kill():
-        print('Killing all images... ', end='', flush=True)
-        call(['docker', 'image', 'prune', '-a', '-f'], stdout=PIPE)
-        print('done')
