82a6867232c3519cd3bf2a92eac160d590b814f8
Yin Keet <yinkeet.wong@imoney-group.com>
2018-10-29 02:29:12 +0800

    Removed unused scripts

--- library/settings.py
+++
@@ -1,8 +0,0 @@
-import json
-import os
-
-def load():
-    dir_path = os.path.dirname(os.path.realpath(__file__)) + '/'
-    with open(dir_path + '../kubesh.json') as data:
-        settings = json.load(data)
-    return settings
