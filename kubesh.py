1f4e29b8c1ab1d8eb1db38e1c30861333d008690
yinkeet <wongyinkeet@gmail.com>
2019-03-04 19:02:58 +0800

    Refactor

--- kubesh.py
+++ kubesh.py
@@ -6,33 +6,23 @@ import sys
 from argparse import ArgumentParser
 from pathlib import Path
 
-dir_path = os.path.dirname(os.path.realpath(__file__)) + '/'
-sys.path.append(dir_path + 'library')
+# dir_path = os.path.dirname(os.path.realpath(__file__)) + '/'
+# sys.path.append(dir_path + 'library')
 
-from argparser import KubeshArgumentParser
-from aks import AKS
-from common import load_environment_file, load_environment_variables
-from docker import Docker
-from kubernetes import Kubernetes
-from gke import GKE
-from minikube import Minikube
-from environment import Environment
+from library.argparser import KubeshArgumentParser
+from library.aks import AKS
+from library.common import get_containers_from_yaml, load_environment_file, load_environment_variables
+from library.docker import Docker
+from library.kubernetes import Kubernetes
+from library.gke import GKE
+from library.minikube import Minikube
+from library.environment import Environment
+from library.config import Config
 
-custom_settings_file = Path("kubesh.json")
-if custom_settings_file.is_file():
-    with open('kubesh.json') as data:
-        settings = json.load(data)
-else:
-    if getattr(sys, 'frozen', False):
-        dir_path = os.path.dirname(os.path.realpath(sys.executable)) + '/'
-    with open(dir_path + 'kubesh.json') as data:
-        settings = json.load(data)
-
-parser = KubeshArgumentParser(settings)
+parser = KubeshArgumentParser(Config().settings)
 args = parser.run()
 
-info = settings['environments'][args.environment]
-load_environment_file(info['environment_filename'])
+Config().environment = args.environment
 
 class_map = {
     'docker': Docker,
@@ -43,10 +33,10 @@ class_map = {
     }
 }
 
-klass = class_map[info['type']]
+klass = class_map[Config().current_setting['type']]
 if isinstance(klass, dict):
-    klass = klass[info['cloud_service']]
-instance = klass(info, settings.get('templates', None))
+    klass = klass[Config().current_setting['cloud_service']]
+instance = klass(Config().current_setting, Config().settings.get('templates', {}))
 method = getattr(instance, args.operation)
 kwargs = vars(args)
 kwargs.pop('environment', None)
--- library/argparser.py
+++ library/argparser.py
@@ -1,71 +1,15 @@
+import os
 import sys
+import yaml
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 from subprocess import call
-
-from common import get_containers, get_services
 
 
 class KubeshArgumentParser():
     def __init__(self, settings):
         self.settings = settings
-        
-    def __setup_environment_parsers(self, parent_parser):
-        subparsers = parent_parser.add_subparsers(title='Environments', dest='environment', required=True, help='Environment')
-        for id, setting in self.settings['environments'].items():
-            parser = subparsers.add_parser(id, help=setting['description'])
-            if type == 'docker':
-                containers = get_services(setting['deployment_filename'])
-            else:
-                containers = get_containers(setting['dockerfile_filename'])
-                containers.extend(setting.get('containers', []))
-            self.__setup_operation_parsers(parser, setting['type'], containers)
 
-    def __setup_operation_parsers(self, parent_parser, type, containers):
-        subparsers = parent_parser.add_subparsers(title='Operations', dest='operation', required=True, help='Operation to execute')
-        # Build
-        parser = subparsers.add_parser('build', help='Build, tag and push image(s).')
-        parser.add_argument('containers', nargs='*', help='Containers to build')
-        # Config
-        parser = subparsers.add_parser('config', help='Show deployment config yaml.')
-        # Image pull secret
-        if type == 'kubernetes':
-            parser = subparsers.add_parser('image_pull_secret', help='Creates an image pull secret.')
-        # Run
-        parser = subparsers.add_parser('run', help='Run deployment.')
-        parser.add_argument('containers', nargs='*', help='Containers to run')
-        # Remove untagged images
-        parser = subparsers.add_parser('clean_up', help='Remove untagged images.')
-        parser.add_argument('containers', nargs='*', help='Containers to clean')
-        # Stop
-        parser = subparsers.add_parser('stop', help='Stop deployment.')
-        parser.add_argument('containers', nargs='*', help='Containers to stop')
-        # Namespace
-        if type != 'docker':
-            parser = subparsers.add_parser('namespace', help='Sets kubectl to the environment\'s namespace.')
-        # Logs
-        parser = subparsers.add_parser('logs', help='Show logs of container in deployment.')
-        if type == 'docker':
-            parser.add_argument('containers', nargs='+', choices=containers, help='Containers to show the logs')    
-        else:
-            parser.add_argument('pod', help='Pod to show the logs')    
-            parser.add_argument('container', choices=containers, help='Container to show the logs')
-        parser.add_argument('-f', '--follow', action='store_true', help='Follow log output')
-        # SSH
-        parser = subparsers.add_parser('ssh', help='Tunnel into container of the deployment.')
-        if type != 'docker':
-            parser.add_argument('pod', help='Pod to tunnel into')
-        parser.add_argument('container', choices=containers, help='Container to tunnel into')
-        parser.add_argument('command', help='Command to execute')
-        parser.add_argument('args', nargs='*', help='Command arguments')
-        # Auth and cluster
-        if type == 'kubernetes':
-            # Auth
-            subparsers.add_parser('auth', help='Authenticate this machine.')
-            # Cluster
-            subparsers.add_parser('cluster', help='Switch cluster.')
-
-    def run(self):
-        parser = ArgumentParser(
+        self.__parser = ArgumentParser(
             formatter_class=RawDescriptionHelpFormatter,
             epilog='''
 Clipboard:
@@ -75,5 +19,68 @@ Clipboard:
     minikube stop && eval $(minikube docker-env -u)
             '''
         )
-        self.__setup_environment_parsers(parser)
-        return parser.parse_args()
+        self.__setup_environment_parsers()
+        
+    def __setup_environment_parsers(self):
+        subparsers = self.__parser.add_subparsers(title='Environments', dest='environment', required=True, help='Environment')
+        for id, setting in self.settings['environments'].items():
+            parser = subparsers.add_parser(id, help=setting['description'])
+            self.__setup_operation_parsers(parser, setting['type'], setting['containers']['all'], setting['containers']['buildable'])
+
+    def __setup_operation_parsers(self, parent_parser, type, all_containers, buildable_containers):
+        subparsers = parent_parser.add_subparsers(title='Operations', dest='operation', required=True, help='Operation to execute')
+        
+        # Build
+        parser = subparsers.add_parser('build', help='Build, tag and push image(s).')
+        parser.add_argument('-c', '--containers', nargs='+', choices=buildable_containers, default=buildable_containers, help='Containers to build')
+        
+        # Config
+        parser = subparsers.add_parser('config', help='Show deployment config yaml.')
+        parser.add_argument('--containers', action='store_const', const=False, default=buildable_containers)
+        
+        # Image pull secret
+        if type == 'kubernetes':
+            parser = subparsers.add_parser('image_pull_secret', help='Creates an image pull secret.')
+        
+        # Run
+        parser = subparsers.add_parser('run', help='Run deployment.')
+        parser.add_argument('-c', '--containers', nargs='+', choices=all_containers, default=all_containers, help='Containers to run')
+        
+        # Remove untagged images
+        parser = subparsers.add_parser('clean_up', help='Remove untagged images.')
+        parser.add_argument('-c', '--containers', nargs='+', choices=all_containers, default=all_containers, help='Containers to clean')
+        
+        # Stop
+        parser = subparsers.add_parser('stop', help='Stop deployment.')
+        parser.add_argument('-c', '--containers', nargs='+', choices=all_containers, default=all_containers, help='Containers to stop')
+        
+        # Namespace
+        if type != 'docker':
+            parser = subparsers.add_parser('namespace', help='Sets kubectl to the environment\'s namespace.')
+        
+        # Logs
+        parser = subparsers.add_parser('logs', help='Show logs of container in deployment.')
+        if type == 'docker':
+            parser.add_argument('containers', nargs='+', choices=all_containers, help='Containers to show the logs')    
+        else:
+            parser.add_argument('pod', help='Pod to show the logs')    
+            parser.add_argument('container', choices=all_containers, help='Container to show the logs')
+        parser.add_argument('-f', '--follow', action='store_true', help='Follow log output')
+        
+        # SSH
+        parser = subparsers.add_parser('ssh', help='Tunnel into container of the deployment.')
+        if type != 'docker':
+            parser.add_argument('pod', help='Pod to tunnel into')
+        parser.add_argument('container', choices=all_containers, help='Container to tunnel into')
+        parser.add_argument('command', help='Command to execute')
+        parser.add_argument('args', nargs='*', help='Command arguments')
+        
+        # Auth and cluster
+        if type == 'kubernetes':
+            # Auth
+            subparsers.add_parser('auth', help='Authenticate this machine.')
+            # Cluster
+            subparsers.add_parser('cluster', help='Switch cluster.')
+
+    def run(self):
+        return self.__parser.parse_args()
